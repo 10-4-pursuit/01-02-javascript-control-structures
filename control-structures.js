4f6becce6ebef0562ba075d7747a065302e078c3
athomas227 <andrewthomas@pursuit.org>
2023-12-04 19:42:30 -0500

    finished exercise

---
+++ control-structures.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 5; i++) {
+    console.log(`Number ${i}`);
+}
+
+const name= "Bob";
+if (name === "Alice") {
+    console.log("Hello Alice!");
+} else if (name === "Bob") {
+    console.log("Hello Bob!")
+} else {
+    console.log("You are not Alice");
+}
